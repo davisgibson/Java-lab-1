cd42b8695e1ec52003e60f4813e21da1b21aade6
davisgibson <juicepluskid2@yahoo.com>
2015-03-06 13:28:44 -0600

    adding file
    sa

---
+++ Lab1.java
@@ -0,0 +1,9 @@
+public class Lab1 {
+	public static void main (String args[]){
+		System.out.println("DD    SSS   GGG");
+		System.out.println("D D  S     G");
+		System.out.println("D  D  SS   G GG");
+		System.out.println("D D     S  G  G");
+		System.out.println("DD   SSS    GG");
+	}
+}
